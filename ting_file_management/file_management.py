99c835965232f2d5681ee1f7a664d55cb66d0b44
Arthur Teixeira <arthursony1@hotmail.com>
2024-02-02 22:42:30 -0300

    req 1 e 2

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,15 @@
+import sys
+
+
 def txt_importer(path_file):
-    """Aqui irá sua implementação"""
+    try:
+        if not path_file.endswith('.txt'):
+            sys.stderr.write('Formato inválido')
+
+        with open(path_file, 'r') as file:
+            content = file.read()
+            lines = content.split('\n')
+            return lines
+
+    except FileNotFoundError:
+        sys.stderr.write(f'Arquivo {path_file} não encontrado\n')
