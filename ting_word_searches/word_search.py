6442ca9defe0fc233f91cbbc3be6befb9797015d
Arthur Teixeira <arthursony1@hotmail.com>
2024-02-04 21:44:48 -0300

    req 7

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,5 +1,23 @@
 def exists_word(word, instance):
-    """Aqui irá sua implementação"""
+    result = []
+    for i in range(len(instance)):
+        ocorrencias = []
+        lines = instance.search(i)["linhas_do_arquivo"]
+        name = instance.search(i)["nome_do_arquivo"]
+        for j in range(len(lines)):
+            if word in lines[j].lower():
+                ocorrencias.append(
+                    {"linha": j + 1}
+                )
+        if ocorrencias:
+            result.append(
+                {
+                    "palavra": word,
+                    "arquivo": name,
+                    "ocorrencias": ocorrencias,
+                }
+            )
+    return result
 
 
 def search_by_word(word, instance):
